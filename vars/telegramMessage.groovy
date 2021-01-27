391c175a8ffc44c33300c6b78c32fddc0ae997ab
huytz <huy3100@gmail.com>
2021-01-27 11:30:32 +0700

    add telegram message

---
+++ vars/telegramMessage.groovy
@@ -0,0 +1,14 @@
+void call(String message='') {
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+
+    withCredentials([string(credentialsId: 'telegramToken', variable: 'TOKEN'),
+    string(credentialsId: 'telegramChatId', variable: 'CHAT_ID')]) {
+
+        response = httpRequest (consoleLogResponseBody: true,
+                contentType: 'APPLICATION_JSON',
+                httpMode: 'GET',
+                url: "https://api.telegram.org/bot$TOKEN/sendMessage?text=$encodedMessage&chat_id=$CHAT_ID&disable_web_page_preview=true",
+                validResponseCodes: '200')
+        return response
+    }
+}
