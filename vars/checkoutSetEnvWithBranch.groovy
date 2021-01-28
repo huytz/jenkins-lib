72898c4b5f84526f5a9a5e48136e6fc6717c19ab
huytz <huy3100@gmail.com>
2021-01-28 22:13:35 +0700

    modify git checkout

--- vars/checkoutSetEnvWithBranch.groovy
+++ vars/checkoutSetEnvWithBranch.groovy
@@ -1,10 +1,17 @@
 void call(String Branch='') {
-    Map scmVars =  Map scmVars = checkout([$class: 'GitSCM', 
-                          branches: [[name: Branch]], 
-                          doGenerateSubmoduleConfigurations: false, 
-                          extensions: [], 
-                          gitTool: 'Default'])
-        
+    Map scmVars =  Map scmVars = checkout scm : [
+					$class: 'GitSCM', 
+                          		branches: [[name: Branch]], 
+                          		doGenerateSubmoduleConfigurations: false, 
+       			  		extensions: [[$class: 'SubmoduleOption',
+                                      		disableSubmodules: false,
+                                      		parentCredentials: false,
+                                      		recursiveSubmodules: true,
+                                      		reference: '',
+                                      		trackingSubmodules: false]],
+                          		submoduleCfg: [],
+                          		userRemoteConfigs: scm.userRemoteConfigs
+				]
     env.GIT_BRANCH = scmVars.GIT_BRANCH
     env.GIT_COMMIT = scmVars.GIT_COMMIT
     env.GIT_PREVIOUS_COMMIT = scmVars.GIT_PREVIOUS_COMMIT
